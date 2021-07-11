c75c993a0534baa88af4bd5d6cb486efbd1db32e
B-Lidberg <76635047+B-Lidberg@users.noreply.github.com>
2021-07-11 15:36:34 -0400

    Initial commit

---
+++ Navigation-Playground/app/src/main/java/com/lid/navigation_playground/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.lid.navigation_playground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.lid.navigation_playground.ui.theme.NavigationPlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationPlaygroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NavigationPlaygroundTheme {
+        Greeting("Android")
+    }
+}
